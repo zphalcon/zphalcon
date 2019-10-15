b795a30681323248325e19256b22a6c1f682bc89
WWWSpider <foxy_8888@163.com>
2019-10-15 11:03:48 +0800

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+require_once(__DIR__ . '/core.php');
+
+spl_autoload_register(function ($class) {
+	if (strpos($class, "Phalcon") === 0 || strpos($class, "\Phalcon") === 0) {
+		include (__DIR__ . '/' . uncamelize($class) . '.php');
+	}
+});
